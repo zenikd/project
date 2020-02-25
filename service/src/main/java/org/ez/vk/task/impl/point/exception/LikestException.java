6179e35bc879bed7b7e9a9be61d5318c53980f00
zenikd@tut.by <rjkjrjkf1997>
2020-02-26 00:33:21 +0300

    Remove earn point task

--- service/src/main/java/org/ez/vk/task/impl/point/exception/LikestException.java
+++
@@ -1,7 +0,0 @@
-package org.ez.vk.task.impl.point.exception;
-
-import org.ez.vk.exception.internal.InternalException;
-
-public class LikestException extends InternalException{
-
-}
--- service/src/main/java/org/ez/vk/task/impl/point/exception/TaskReservedException.java
+++
@@ -1,5 +0,0 @@
-package org.ez.vk.task.impl.point.exception;
-
-public class TaskReservedException extends LikestException {
-
-}
